a01ae42f8081df6289838bb6261060463eba7902
Xenocider <lhw120823@163.com>
2021-03-20 07:57:38 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+ 
+setup(
+  name='xenoslib',
+  version='0.01',
+  packages=find_packages(),
+  description="Xenos' common lib",
+  author='Xenocider',
+  author_email='xenos.lu@gmail.com',
+  url="None",
+)
