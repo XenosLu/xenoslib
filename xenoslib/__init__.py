4f31a96234825713d804a2d34197e042e063e71e
Xenocider <lhw120823@163.com>
2021-03-20 08:57:53 +0800

    feat

--- xenoslib/__init__.py
+++ xenoslib/__init__.py
@@ -1,3 +1,5 @@
+from .common import RestartSelfIfUpdated
+
 class SingletonWithArgs:
     """带参数的单例模式, 通过继承使用，需放到第一继承位"""
     def __new__(cls, *args, **kwargs):
@@ -7,3 +9,4 @@ class SingletonWithArgs:
         if not cls._instances.get(arg):
             cls._instances[arg] = super().__new__(cls)
         return cls._instances[arg]
+
---
+++ xenoslib/common.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import os
+
+class RestartSelfIfUpdated:
+    """检测自身更新并重启"""
+    mtime = 0
+
+    def __init__(self):
+        mtime = os.path.getmtime(__file__)
+        if self.mtime and mtime != self.mtime:
+            print(
+                __file__,
+                'mtime changed:',
+                time.strftime('%H%M', time.localtime(self.mtime)),
+                'to',
+                time.strftime('%H%M', time.localtime(mtime)),
+                ', restarting...',
+            )
+            self.restart()
+        RestartSelfIfUpdated.mtime = os.path.getmtime(__file__)
+
+    def restart(self):
+        python = sys.executable
+        os.execl(python, python, *sys.argv)
