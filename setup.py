4d5a1b979489f99c5270072d31d1ce8080301c21
Xenocider <lhw120823@163.com>
2021-03-20 10:01:46 +0800

    fix

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
  
 setup(
   name='xenoslib',
-  version='0.1.2',
+  version='0.1.2.1',
   packages=find_packages(),
   description="Xenos' common lib",
   author='Xenocider',
