edd67295ad6f4e0e6cd4072e48c681021355fdb5
Xenocider <lhw120823@163.com>
2021-03-20 10:15:22 +0800

    refactor

--- xenoslib/__init__.py
+++ xenoslib/__init__.py
@@ -1,14 +1,3 @@
+from .base import *
 from .common import *
 from .extend import *
-
-
-class SingletonWithArgs:
-    """带参数的单例模式, 通过继承使用，需放到第一继承位"""
-
-    def __new__(cls, *args, **kwargs):
-        arg = '%s%s' % (args, kwargs)
-        if not hasattr(cls, '_instances'):
-            cls._instances = {}
-        if not cls._instances.get(arg):
-            cls._instances[arg] = super().__new__(cls)
-        return cls._instances[arg]
