26b7bef483c0ac93bd3a06c12a979acf3b1a26d7
Xenocider <lhw120823@163.com>
2021-03-20 09:59:10 +0800

    feat

--- setup.py
+++ setup.py
@@ -4,10 +4,11 @@ from setuptools import setup, find_packages
  
 setup(
   name='xenoslib',
-  version='0.1.1',
+  version='0.1.2',
   packages=find_packages(),
   description="Xenos' common lib",
   author='Xenocider',
   author_email='xenos.lu@gmail.com',
   url="None",
+  install_requires=['PyYAML>=5.1'],
 )
