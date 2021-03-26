5fcd0a17b3129a6839bf9f1525da00c796a346fc
Xenocider <lhw120823@163.com>
2021-03-26 20:40:01 +0800

    perf: dependcies

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='xenoslib',
-    version='0.1.2.3',
+    version='0.1.2.4',
     packages=find_packages(),
     description="Xenos' common lib",
     author='Xenocider',
     author_email='xenos.lu@gmail.com',
     url='https://github.com/XenosLu/xenoslib.git',
-    install_requires=['PyYAML>=5.1'],
+    install_requires=['PyYAML==5.4'],
     python_requires='>=3.4.0',
 )
