8a7bac7c1fb1040738c3e0d3d84b4149cbe566e7
Xenocider <xenos.lu@gmail.com>
2021-11-10 14:16:28 +0800

    misc

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ import xenoslib
 
 setup(
     name='xenoslib',
-    # version='0.1.2.4',
     version=xenoslib.__version__,
     packages=find_packages(),
     description="Xenos' common lib",
@@ -14,4 +13,10 @@ setup(
     url='https://github.com/XenosLu/xenoslib.git',
     install_requires=['PyYAML>=5.4'],
     python_requires='>=3.4.0',
+    extras_require={
+        ':sys_platform == "win32"': [
+            'pywin32>=223'
+        ],
+        ':"linux" in sys_platform': []
+    },
 )
