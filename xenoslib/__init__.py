bdda27f0a85cd9b75a15c4d00a5fcbe87683ffc5
Xenocider <lhw120823@163.com>
2022-03-16 09:07:46 +0800

    feat: win tray icon

--- xenoslib/__init__.py
+++ xenoslib/__init__.py
@@ -1,10 +1,11 @@
 import sys
 if sys.platform == 'win32':
     from .windows import *
+    from .win_trayicon import *
 else:
     from .linux import *
 from .base import *
 from .dev import *
 from .dev import RestartWhenModified as RestartSelfIfUpdated  # deprecated class name
 from .extend import *
-__version__ = '0.1.6.6'
+__version__ = '0.1.7.0'
