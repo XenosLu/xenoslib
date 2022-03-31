1f8c780dc5da4b8869e42778e6d64de09b7aef78
Xenocider <lhw120823@163.com>
2022-03-31 14:30:40 +0800

    misc

--- publish.cmd
+++ publish.cmd
@@ -3,6 +3,7 @@ REM python setup.py sdist
 set current=%~dp0
 cd /d %current%
 
+flake8 . --count --max-complexity=10 --max-line-length=127 --statistics || goto :END
 python tests\unit_test.py || goto :END
 
 rd /s /q dist
