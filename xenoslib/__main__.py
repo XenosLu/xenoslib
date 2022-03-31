c32c9522687f153bd4e90c9650a3bd7348306975
Xenocider <lhw120823@163.com>
2022-03-31 10:44:45 +0800

    format

--- xenoslib/__main__.py
+++ xenoslib/__main__.py
@@ -5,9 +5,11 @@ import xenoslib
 
 __version__ = importlib.metadata.version('xenoslib')
 
+
 def main():
     print(inspect.getsourcefile(xenoslib))
     print(__version__)
+
 
 if __name__ == '__main__':
     main()
