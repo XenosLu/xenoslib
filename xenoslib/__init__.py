ab99966c23e4b37f30b9729bee1346265a75eafc
Xenocider <lhw120823@163.com>
2022-04-01 16:53:41 +0800

    format: black

--- xenoslib/__init__.py
+++ xenoslib/__init__.py
@@ -1,6 +1,7 @@
 import sys
 
 from .version import __version__  # noqa
+
 if sys.platform == 'win32':
     from .windows import *  # noqa
     from .win_trayicon import *  # noqa
@@ -8,4 +9,6 @@ else:
     from .linux import *  # noqa
 from .base import *  # noqa
 from .dev import *  # noqa
-from .dev import RestartWhenModified as RestartSelfIfUpdated  # deprecated class name countdown 2  # noqa
+from .dev import (
+    RestartWhenModified as RestartSelfIfUpdated,
+)  # deprecated class name countdown 1  # noqa
