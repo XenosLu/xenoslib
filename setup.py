57e0d4c48c716e1250085d109947318cf60ec9df
Xenocider <xenos.lu@gmail.com>
2022-04-07 07:33:24 +0800

    perf: req

--- setup.py
+++ setup.py
@@ -18,12 +18,11 @@ setup(
     url='https://github.com/XenosLu/xenoslib.git',
     install_requires=[
         'PyYAML>=5.4',
-        # 'requests==2.2',
     ],
     python_requires='>=3.6',
     extras_require={
         ':sys_platform == "win32"': ['pywin32>=225'],
-        ':python_version >= "3.10"': ['requests==2.19'],  # 2.18 - 2.21
+        ':python_version >= "3.10"': ['requests>=2.19'],
         ':python_version <= "3.9"': ['requests>=2.0.0'],
         'colorful:sys_platform == "win32"': ['colorama>=0.4.4'],
         ':"linux" in sys_platform': [],
