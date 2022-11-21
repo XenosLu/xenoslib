2433f619222c33c373a4270111562cd520e4230d
Xenocider <xenos.lu@gmail.com>
2022-11-21 16:44:30 +0800

    perf: compatibility

--- xenoslib/__init__.py
+++ xenoslib/__init__.py
@@ -5,6 +5,6 @@ from .about import __version__  # noqa
 if sys.platform == "win32":
     from .windows import *  # noqa
     from .win_trayicon import *  # noqa
-else:
+elif sys.platform == "linux":
     from .linux import *  # noqa
 from .base import *  # noqa
