eaab3bcdad27ccd961210b7efd8c41a87dddc36d
Xenocider <xenos.lu@gmail.com>
2022-11-30 13:38:02 +0800

    feat: add windows path env

--- xenoslib/windows.py
+++ xenoslib/windows.py
@@ -5,6 +5,46 @@ import sys
 import ctypes
 import time
 import msvcrt
+import winreg
+
+
+def refresh():
+    """refresh after update registry"""
+    HWND_BROADCAST = 0xFFFF
+    WM_SETTINGCHANGE = 0x1A
+    SMTO_ABORTIFHUNG = 0x0002
+
+    result = ctypes.c_long()
+    ctypes.windll.user32.SendMessageTimeoutW(
+        HWND_BROADCAST,
+        WM_SETTINGCHANGE,
+        0,
+        "Environment",
+        SMTO_ABORTIFHUNG,
+        5000,
+        ctypes.byref(result),
+    )
+
+
+def add_windows_path_env(new_path):
+    """add directory to Windows path environment variable"""
+    print("add dircetory to path: %s" % new_path)
+    path_key = winreg.OpenKey(
+        winreg.HKEY_LOCAL_MACHINE,
+        r"SYSTEM\CurrentControlSet\Control\Session Manager\Environment",
+        0,
+        winreg.KEY_ALL_ACCESS,
+    )
+    path_str, _ = winreg.QueryValueEx(path_key, "Path")
+    path_list = path_str.split(";")
+    if new_path in path_list:
+        print("%s exists" % new_path)
+    else:
+        print("added %s" % new_path)
+        path_list.append(new_path)
+        new_path = ";".join(path_list)
+        winreg.SetValueEx(path_key, "Path", 0, winreg.REG_EXPAND_SZ, new_path)
+        # refresh()
 
 
 class RunAsAdmin:
