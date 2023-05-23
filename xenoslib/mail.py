a4eb73759a9ebe385fa0de57c37da57a5a0f0653
Xenocider <xenos.lu@gmail.com>
2023-05-23 10:24:20 +0800

    Update mail.py

--- xenoslib/mail.py
+++ xenoslib/mail.py
@@ -15,11 +15,11 @@ logger = logging.getLogger(__name__)
 
 class MailFetcher:
     """
-    keep fetching from mail inbox using IMAP protocol
-    usage:
+    Fetch emails from mail inbox using IMAP protocol.
+    Usage:
 
-    for mail in MailFetcher(imap_server, mail_addr, mail_pwd, interval=30, days=1):
-        print(mail)
+    for email_data in MailFetcher(imap_server, mail_addr, mail_pwd, interval=1, days=33):
+        print(email_data["subject"])
     """
 
     msg_ids = deque(maxlen=999)
@@ -33,48 +33,48 @@ class MailFetcher:
         return self.fetching(interval=interval)
 
     def fetching(self, interval=30):
-        """keep fetching every interval"""
-        logger.debug("start checking mails...")
+        """Continuously fetch emails at the specified interval."""
+        logger.debug("Start checking emails...")
         while True:
             try:
-                yield from self.mail_parse_generator(self.fetch_mails())
+                yield from self.parse_emails(self.fetch_emails())
             except Exception as exc:
                 logger.warning(exc)
             sleep(interval)
 
-    def mail_parse_generator(self, mails):
-        for msg_id, msg in mails.items():
+    def parse_emails(self, emails):
+        for msg_id, msg in emails.items():
             if msg_id in self.msg_ids:
                 continue
             body = email.message_from_bytes(msg[b"BODY[]"])
             subject = str(email.header.make_header(email.header.decode_header(body["Subject"])))
-            from_ = body["From"]
+            sender = body["From"]
             date = body["Date"]
             payload = body.get_payload(decode=True)
             if payload:
                 payload = payload.decode()
             internal_date = msg[b"INTERNALDATE"]
-            msg = {
+            email_data = {
                 "body": body,
                 "subject": subject,
                 "payload": payload,
                 "date": date,
-                "from": from_,
+                "sender": sender,
                 "internal_date": internal_date,
             }
-            yield msg
+            yield email_data
             self.msg_ids.append(msg_id)
 
-    def fetch_mails(self):
-        """login and fetch mails once"""
-        logger.debug(f"fetching mails in {self.days} day(s)...")
+    def fetch_emails(self):
+        """Login and fetch emails."""
+        logger.debug(f"Fetching emails from the past {self.days} day(s)...")
         date_str = datetime.datetime.today() - datetime.timedelta(days=self.days)
         with IMAPClient(self.imap_server, timeout=30) as client:
             client.login(self.mail_addr, self.mail_pwd)
             client.select_folder("INBOX", readonly=True)
             messages = client.search(["SINCE", date_str])
-            mails = client.fetch(messages, ["INTERNALDATE", "BODY.PEEK[]"])
-            return mails
+            emails = client.fetch(messages, ["INTERNALDATE", "BODY.PEEK[]"])
+            return emails
 
 
 def test():
@@ -85,8 +85,8 @@ def test():
     imap_server = os.environ["imap_server"]
     mail_addr = os.environ["imap_addr"]
     mail_pwd = os.environ["imap_pass"]
-    for mail in MailFetcher(imap_server, mail_addr, mail_pwd, interval=1, days=33):
-        print(mail["subject"])
+    for email_data in MailFetcher(imap_server, mail_addr, mail_pwd, interval=1, days=33):
+        print(email_data["subject"])
 
 
 if __name__ == "__main__":
