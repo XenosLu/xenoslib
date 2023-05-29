ff45345f092deb06a4d793dfe7d9d48a52bc43a1
Xenocider <xenos.lu@gmail.com>
2023-05-29 10:15:08 +0800

    feat: windows.Environment; rewrite add_windows_path_env

--- xenoslib/windows.py
+++ xenoslib/windows.py
@@ -8,52 +8,6 @@ import msvcrt
 import winreg
 
 
-def refresh_update_env():
-    """refresh after update environment"""
-    HWND_BROADCAST = 0xFFFF
-    WM_SETTINGCHANGE = 0x1A
-    SMTO_ABORTIFHUNG = 0x0002
-
-    result = ctypes.c_long()
-    ctypes.windll.user32.SendMessageTimeoutW(
-        HWND_BROADCAST,
-        WM_SETTINGCHANGE,
-        0,
-        "Environment",
-        SMTO_ABORTIFHUNG,
-        5000,
-        ctypes.byref(result),
-    )
-
-
-def add_windows_path_env(new_path):
-    """Add directory to Windows path environment variable"""
-    print(f"Adding directory to path: {new_path}")
-    path_key = winreg.OpenKey(
-        winreg.HKEY_LOCAL_MACHINE,
-        r"SYSTEM\CurrentControlSet\Control\Session Manager\Environment",
-        0,
-        winreg.KEY_ALL_ACCESS,
-    )
-    path_str, _ = winreg.QueryValueEx(path_key, "Path")
-    path_list = path_str.split(";")
-    if new_path in path_list:
-        print(f"{new_path} already exists in the path")
-        return False
-    else:
-        print(f"Added {new_path} to the path")
-        path_list.append(new_path)
-        new_path = ";".join(path_list)
-        winreg.SetValueEx(path_key, "Path", 0, winreg.REG_EXPAND_SZ, new_path)
-        try:
-            winreg.SetValueEx(path_key, "Path", 0, winreg.REG_EXPAND_SZ, new_path)
-            refresh_update_env()
-            return True
-        except Exception as exc:
-            print(f"Failed to update the path: {str(exc)}")
-            return False
-
-
 class RunAsAdmin:
     """
     Usage: RunAsAdmin(main, cmd=True)
@@ -107,11 +61,75 @@ def timeout(seconds):
     print()  # make sure the message won't be covered
 
 
+class Environment:
+    reg_path = r"SYSTEM\CurrentControlSet\Control\Session Manager\Environment"
+
+    def __init__(self):
+        pass
+
+    def refresh(self):
+        """refresh environment after updated"""
+        HWND_BROADCAST = 0xFFFF
+        WM_SETTINGCHANGE = 0x1A
+        SMTO_ABORTIFHUNG = 0x0002
+
+        result = ctypes.c_long()
+        ctypes.windll.user32.SendMessageTimeoutW(
+            HWND_BROADCAST,
+            WM_SETTINGCHANGE,
+            0,
+            "Environment",
+            SMTO_ABORTIFHUNG,
+            5000,
+            ctypes.byref(result),
+        )
+
+    def set(self, key, value):
+        print(f"Setting [{key}] to windows environment...")
+        with winreg.OpenKey(
+            winreg.HKEY_LOCAL_MACHINE, self.reg_path, 0, winreg.KEY_ALL_ACCESS
+        ) as reg_key:
+            winreg.SetValueEx(reg_key, key, 0, winreg.REG_EXPAND_SZ, value)
+            self.refresh()
+            print(f"Setted [{key}] to windows environment.")
+
+    def get(self, key):
+        with winreg.OpenKey(
+            winreg.HKEY_LOCAL_MACHINE, self.reg_path, 0, winreg.KEY_ALL_ACCESS
+        ) as reg_key:
+            value, _ = winreg.QueryValueEx(reg_key, key)
+            return value
+
+    def update(self, environs):
+        for key, value in environs.items():
+            self.set(key, value)
+            # print(key, value)
+
+
+def add_windows_path_env(new_path):
+    """Add directory to Windows path environment variable"""
+    env = Environment()
+    path_str = env.get("Path")
+    path_list = path_str.split(";")
+    if new_path in path_list:
+        print(f"{new_path} already exists in the path, skip.")
+        return False
+    else:
+        path_list.append(new_path)
+        new_path_list = ";".join(path_list)
+        try:
+            env.set("Path", new_path_list)
+            print(f"Added {new_path} to the path")
+            return True
+        except Exception as exc:
+            print(f"Failed to update the path: {str(exc)}")
+            return False
+
+
 def test():
-    add_windows_path_env('c:\\falloutx')
-    print("added")
+    add_windows_path_env("c:\\abcde")
     pause()
+
 
 if __name__ == "__main__":
     RunAsAdmin(test, cmd=True)
-
