02b8fb216a1b01f9fe568876d56776d25efa86bb
XenosLu <xenos.lu@gmail.com>
2023-05-30 05:52:46 +0000

    format: Auto Format with Black

--- xenoslib/mail.py
+++ xenoslib/mail.py
@@ -74,10 +74,10 @@ class MailFetcher:
             if payload:
                 payload = payload.decode()
             internal_date = msg[b"INTERNALDATE"]
-            
-            body['subject'] = subject
-            body['payload'] = payload
-            body['internal_date'] = internal_date
+
+            body["subject"] = subject
+            body["payload"] = payload
+            body["internal_date"] = internal_date
             yield body
             self.msg_ids.append(msg_id)
 
